d78ad9ae92b39f2ac968557a67bdf75846f2bc34
thaihn <thai.hn@altplus.com.vn>
2019-07-22 15:07:52 +0700

    Add Preferences to project

--- app/src/main/java/com/example/weathersample/ui/main/MainActivityModule.kt
+++ app/src/main/java/com/example/weathersample/ui/main/MainActivityModule.kt
@@ -1,6 +1,7 @@
 package com.example.weathersample.ui.main
 
 import androidx.lifecycle.ViewModelProvider
+import com.example.base.data.prefs.AppPreferenceHelper
 import com.example.base.repository.weather.WeatherRepository
 import com.example.base.ui.ViewModelProviderFactory
 import dagger.Module
@@ -10,8 +11,8 @@ import dagger.Provides
 class MainActivityModule {
 
     @Provides
-    internal fun provideMainViewModel(weatherRepository: WeatherRepository): MainViewModel {
-        return MainViewModel(weatherRepository)
+    internal fun provideMainViewModel(weatherRepository: WeatherRepository, appPreferenceHelper: AppPreferenceHelper): MainViewModel {
+        return MainViewModel(weatherRepository, appPreferenceHelper)
     }
 
     @Provides
